1a14cb8690adc1e49fe8f2c4379d6b882d2164e8
Shreyan <shreyanhaldankar@gmail.com>
2023-02-17 22:12:15 +0530

    A star algo modified n fixed

--- AI/Graph_Problems/AstarAlgo.py
+++ AI/Graph_Problems/AstarAlgo.py
@@ -1,86 +1,91 @@
 from collections import deque
 
 
-#  Make a ds for graph 
+#  Make a ds for graph
 class Graph:
     def __init__(self, adjacency_list):
         self.adjacency_list = adjacency_list
-    
-    def get_neighbours(self, v):
+
+    def get_neighbors(self, v):
         return self.adjacency_list[v]
-    
-    def h(self, n):
+
+    def heuristic_value(self, n):
         H = {
-            'A':1,
-            'B':1,
-            'C':1,
-            'D':1,
-        }
+        "S": 10,
+        "A": 5,
+        "B": 6,
+         "C":4,
+         "D":15,
+         "Y":8,
+         "Z":5,
+         "E":0
+      }
         return H[n]
 
     def a_star_algo(self, start_node, end_node):
         open_list = set([start_node])
         closed_list = set([])
-
+        # For distances
         g = {}
         g[start_node] = 0
-
         parents = {}
         parents[start_node] = start_node
-        
-        while len(open_list)>0:
-            
-            n = None
 
+        while len(open_list) > 0:
+            n = None
+            # N keeps track of current node
             for v in open_list:
-                if n==None or g[v] + self.h(v) < g[n] + self.h(n):
+                if n == None or g[v] + self.heuristic_value(v) < g[n] + self.heuristic_value(n):
                     n = v
-            
+
             if n == None:
                 print("Path does not exist")
                 return None
 
             if n == end_node:
-                reconstructed_path = []
+                recons_path = []
 
-                if parents[n] != n:
-                    reconstructed_path.append(n)
+                while parents[n] != n:
+                    recons_path.append(n)
                     n = parents[n]
-                reconstructed_path.append(start_node)
+                recons_path.append(start_node)
+                recons_path.reverse()
+                print(f"{recons_path}")
+                return recons_path
 
-                reconstructed_path.reverse()
-                print(f"{reconstructed_path}")
-                return reconstructed_path
-
-            for (m, weight) in self.get_neighbours(n):
-
+            for (m, weight) in self.get_neighbors(n):
                 if m not in open_list and m not in closed_list:
                     open_list.add(m)
                     parents[m] = n
                     g[m] = g[n] + weight
-                # if we can reach n first then m
                 else:
+                    # if it is open list or closed list
+                    # then check if it is quicker to reach n first then n
                     if g[m] > g[n] + weight:
                         g[m] = g[n] + weight
                         parents[m] = n
-
+                        # and if m is in closed list remove from closed list and add it in open
                         if m in closed_list:
                             closed_list.remove(m)
                             open_list.add(m)
-
             open_list.remove(n)
             closed_list.add(n)
+
         print("Path not found")
         return None
 
 
-# Testing 
+# Testing
 
 adjacency_list_main = {
-  'A': [('B', 1), ('C', 3), ('D', 7)],
-  'B': [('D', 5)],
-  'C': [('D', 12)]
-  }
+    'S': [('A', 1), ('B', 2)],
+    'A': [('Y', 7), ('Z', 4)],
+    'B': [('C', 7),('D', 1)],
+    'Y': [('E',3)],
+    'Z': [('E', 2)],
+    'C': [('E', 5)],
+    'D': [('E', 12)],
+}
 
 myGraph = Graph(adjacency_list_main)
-myGraph.a_star_algo("A","D")
+myGraph.a_star_algo("S", "E")
